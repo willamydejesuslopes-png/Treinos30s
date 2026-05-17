35936edd3f50b4cfef8f54647bf5f1b34e0ac3da
willamydejesuslopes-png <willamydejesuslopes@gmail.com>
2026-05-17 00:56:35 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'treino30s-v1';
+const urlsToCache = [
+  '/Treinos30s/',
+  '/Treinos30s/index.html',
+  '/Treinos30s/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
